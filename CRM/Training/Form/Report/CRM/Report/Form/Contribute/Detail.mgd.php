5f57dd9a2ef5b49d923ffc6b6d16c0597b6bae48
Deepak Srivastava <deepak.srivastava.0303@gmail.com>
2016-10-05 10:26:41 +0100

    initial commit

---
+++ CRM/Training/Form/Report/CRM/Report/Form/Contribute/Detail.mgd.php
@@ -0,0 +1,21 @@
+<?php
+// This file declares a managed database record of type "ReportTemplate".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+return array (
+  0 => 
+  array (
+    'name' => 'CRM_Training_Form_Report_CRM_Report_Form_Contribute_Detail',
+    'entity' => 'ReportTemplate',
+    'params' => 
+    array (
+      'version' => 3,
+      'label' => 'CRM_Report_Form_Contribute_Detail',
+      'description' => 'CRM_Report_Form_Contribute_Detail (uk.co.vedaconsulting.training)',
+      'class_name' => 'CRM_Training_Form_Report_CRM_Report_Form_Contribute_Detail',
+      'report_url' => 'uk.co.vedaconsulting.training/crm_report_form_contribute_detail',
+      'component' => 'CiviContribute',
+    ),
+  ),
+);
